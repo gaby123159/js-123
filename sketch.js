dc667cb28883bedb0afdbdb1d8474dcc458cc6cb
gaby123159 <00001124196250SP@aluno.educacao.sp.gov.br>
2024-09-12 18:58:54 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+ createCanvas (63, 600);
+ background("black");
+ style.css
+}
+ function draw() {
+ stroke("blue")
+ fill("red");
+   
+ if (mouse-IsPressed){}
+rect(mousex, mousey, 20, 35);
+{
+
+}
